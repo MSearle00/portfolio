835d1f61318497f3bc7bec094db1a98fc1db4087
Maria Searle <maria.searle@deeperthanblue.co.uk>
2020-12-31 18:40:34 +0000

    Updates

---
+++ precache-manifest.13b0924b3b95baddc2a1c271f3acc2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563f8088615bf1475832a22ffc3a6f1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1a24ac58ef9dd7bf800",
+    "url": "/portfolio/static/css/main.49251901.chunk.css"
+  },
+  {
+    "revision": "2dbfaf1d83f50558b69f",
+    "url": "/portfolio/static/js/2.8bde313a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.8bde313a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a24ac58ef9dd7bf800",
+    "url": "/portfolio/static/js/main.a38ddee4.chunk.js"
+  },
+  {
+    "revision": "bcd29fd5c15690c5fb44",
+    "url": "/portfolio/static/js/runtime-main.a4855db3.js"
+  }
+]);
