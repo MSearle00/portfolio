8552e3ac576c28814dcf0f93fb382da25714f05d
Maria Searle <maria.searle@deeperthanblue.co.uk>
2021-01-01 17:17:48 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c97104ff5cd6fab0f704aaab7dc85102.js"
+  "/portfolio/precache-manifest.6d1724406710437c26b3356f9d5217e1.js"
 );
 
 self.addEventListener('message', (event) => {
