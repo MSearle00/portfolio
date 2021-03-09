5c65d03bf8fcba42c0f9f6fbec5f47e6e5f5d6a9
MSearle00 <mariasearle15@gmail.com>
2021-03-09 01:07:43 +0000

    Updates

--- precache-manifest.bc236029ad4a87ab6d3218dc919e9458.js
+++ precache-manifest.bc236029ad4a87ab6d3218dc919e9458.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d20a42363b80f232b1694a8b0b93e20c",
+    "revision": "bb71f3f49ad1ebb3116955eb8f56cca0",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "aae4c5797fc638f507a5",
-    "url": "/portfolio/static/css/main.bff205bf.chunk.css"
+    "revision": "ed4455aed3af244e27a5",
+    "url": "/portfolio/static/css/main.09f3c2bc.chunk.css"
   },
   {
     "revision": "ff161cb1a844df1d5a50",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4cf9a51b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aae4c5797fc638f507a5",
-    "url": "/portfolio/static/js/main.e0e7b1b4.chunk.js"
+    "revision": "ed4455aed3af244e27a5",
+    "url": "/portfolio/static/js/main.f1db64cc.chunk.js"
   },
   {
     "revision": "bcd29fd5c15690c5fb44",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4a62c3b26ccd0bd1397c26e8d8eb7ad5.js"
+  "/portfolio/precache-manifest.bc236029ad4a87ab6d3218dc919e9458.js"
 );
 
 self.addEventListener('message', (event) => {
