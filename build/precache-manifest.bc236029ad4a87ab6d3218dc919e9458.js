4ed0442ee14f6fae13dc188408baf98737d90cde
MSearle00 <mariasearle15@gmail.com>
2021-03-09 01:11:27 +0000

    setting up and publishing on own windows laptop

--- build/precache-manifest.bc236029ad4a87ab6d3218dc919e9458.js
+++ build/precache-manifest.bc236029ad4a87ab6d3218dc919e9458.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "895c39c50459641e69f5989884bd2caa",
+    "revision": "bb71f3f49ad1ebb3116955eb8f56cca0",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "22583a8085324f3d6cc6",
-    "url": "/portfolio/static/css/main.90cd1e95.chunk.css"
+    "revision": "ed4455aed3af244e27a5",
+    "url": "/portfolio/static/css/main.09f3c2bc.chunk.css"
   },
   {
     "revision": "ff161cb1a844df1d5a50",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4cf9a51b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "22583a8085324f3d6cc6",
-    "url": "/portfolio/static/js/main.55763285.chunk.js"
+    "revision": "ed4455aed3af244e27a5",
+    "url": "/portfolio/static/js/main.f1db64cc.chunk.js"
   },
   {
     "revision": "bcd29fd5c15690c5fb44",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6d1724406710437c26b3356f9d5217e1.js"
+  "/portfolio/precache-manifest.bc236029ad4a87ab6d3218dc919e9458.js"
 );
 
 self.addEventListener('message', (event) => {
