afb5e04b7b300a08c5ccf5471400b1d20ec1ec78
MSearle00 <mariasearle15@gmail.com>
2021-03-29 15:42:09 +0100

    Updated about me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -12,62 +12,66 @@ export default function AboutMe() {
           <br></br>
 					<p> I hope to continually improve my skills and knowledge, through both the front end and backend.</p>
           <br></br>
-        <p>At the moment I'm studying C++!</p>
+          <p>At the moment I'm studying C++ and C#!</p>
 
         </div>
 
           {/* SKILLS */}
-          <div class="gridWrapper">
-          <h4 class="subtitle">MY SKILLS</h4>
-            <div class="skills">
-              <ul class="front-end-skills">
-                <strong>FRONT-END:</strong>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-              </ul>
-              <ul class="back-end-skills">
-                <strong>BACK-END: </strong>
-                <li>Redux</li>
-                <li>API Calls</li>
-                <li>Express</li>
-                <li>Mongo</li>
-              </ul>
+          <div class="gridContainer">
+            <div class="gridWrapper">
+            <h4 class="subtitle">MY SKILLS</h4>
+              <div class="skills">
+                <ul class="front-end-skills">
+                  <strong>FRONT-END:</strong>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                </ul>
+                <ul class="back-end-skills">
+                  <strong>BACK-END: </strong>
+                  <li>Redux</li>
+                  <li>API Calls</li>
+                  <li>Express</li>
+                  <li>Mongo</li>
+                  <li>Basic SQL</li>
+                </ul>
+              </div>
             </div>
-          </div>
 
 
-          {/* TECHNOLOGIES */}
-          <div class="gridWrapper">
-            <h4 class="subtitle"> TECHNOLOGIES</h4>
-            <div class="technologies">
-            <ul>
-              <li> GIT </li>
-              <li> Sourcetree</li>
-              <li>Github</li>
-              <li>Bitbucket</li>
-              <li>MongoDB Compass & Atlas</li>
-              <li>Postman</li>
-              <li>VScode</li>
-              <li>SpringToolSuite4</li>
-              <li>Docker</li>
-              <li>Atlassian Group (Jira, Tempo, Confluence)</li>
-            </ul>
-            </div>
-				  </div>
-
-
-          {/* EDUCATION */}
-          <div class="gridWrapper">
-            <h4 class="subtitle"> EDUCATION / QUALIFICATIONS</h4>
-            <div class="education">
+            {/* TECHNOLOGIES */}
+            <div class="gridWrapper">
+              <h4 class="subtitle"> TECHNOLOGIES</h4>
+              <div class="technologies">
               <ul>
-                <li> The Developer Academy, Part Time Bootcamp || 2020 - 2021</li>
-                <li> HNC Computing - Merit || Sheffield College || 2019 - 2020</li>
-                <li> A Level Computing - E || King Edwards VII || 2017 - 2019 </li>
-                <li> GCSE Computing - A || King Edwards VII || 2015 - 2017 </li>
+                <li> GIT </li>
+                <li> Sourcetree</li>
+                <li>Github</li>
+                <li>Bitbucket</li>
+                <li>MongoDB Compass & Atlas</li>
+                <li>Postman</li>
+                <li>Visual Studio Code</li>
+                <li>Visual Studio 2019</li>
+                <li>SpringToolSuite4</li>
+                <li>Docker</li>
+                <li>Atlassian Group (Jira, Tempo, Confluence)</li>
               </ul>
+              </div>
+            </div>
+
+
+            {/* EDUCATION */}
+            <div class="gridWrapper">
+              <h4 class="subtitle"> EDUCATION / QUALIFICATIONS</h4>
+              <div class="education">
+                <ul>
+                  <li> The Developer Academy, Part Time Bootcamp || 2020 - 2021</li>
+                  <li> HNC Computing - Merit || Sheffield College || 2019 - 2020</li>
+                  <li> A Level Computing - E || King Edwards VII || 2017 - 2019 </li>
+                  <li> GCSE Computing - A || King Edwards VII || 2015 - 2017 </li>
+                </ul>
+              </div>
             </div>
           </div>
 
